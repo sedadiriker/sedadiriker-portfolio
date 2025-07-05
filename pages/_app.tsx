ecf2af43d967644fc0b133066c8800e56991abf0
Seda <seda.diriker@gmail.com>
2025-07-05 12:23:43 +0300

    script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Google Analytics script'i ekle
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-159HGM37XR";
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-159HGM37XR', {
+        page_path: window.location.pathname,
+      });
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
   return <Component {...pageProps} />;
 }
